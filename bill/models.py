0672a392b1a285206fc8132efc0ff74efefd457e
Peter Yao <peteryao916@gmail.com>
2014-10-04 02:33:13 -0700

    Updated bill models. No errors. yay

--- bill/models.py
+++ bill/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from datetime import datetime
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+from core.models import TimeStampedModel
+from unit.models import Unit
+
+class Bill(TimeStampedModel):
+    user = models.ForeignKey(User)
+    value = models.DecimalField(max_digits=7, decimal_places=2)
+    debt_type = models.CharField(max_length=24, choices=(('Rent', 'Rent'), ('Utilities', 'Utilities')), default='Rent')
+
+    def __unicode__(self):
+        return self.name
