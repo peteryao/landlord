4bb7d26d58740de1bcbc189030af485ab1760c4c
Peter Yao <peteryao916@gmail.com>
2014-10-04 04:35:07 -0700

    Updated base/management templates

--- core/views.py
+++ core/views.py
@@ -4,8 +4,16 @@ from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.models import User, Permission
 from django.contrib.auth.decorators import login_required
 
+from mgmt.models import Landlord
+from unit.models import Tenant, Unit
+
 # Create your views here.
 def index(request):
     context = {}
-
+    if Landlord.objects.filter(user=request.user).exists():
+        landlord = Landlord.objects.get(user=request.user)
+        context['units'] = Unit.objects.filter(apartment=landlord.apartment)
+        return render(request, 'mgmt/index.html', context)
+    if Tenant.objects.filter(user=request.user).exists():
+        pass
     return render(request, 'core/index.html', context)
