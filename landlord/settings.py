8bbf9a3b7821bd94aba9e579c60d305c66be4894
Peter Yao <peteryao916@gmail.com>
2014-10-04 02:07:22 -0700

    Updated settings.

--- landlord/settings.py
+++ landlord/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'landlord.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'landlord',
+        'USER': 'landlord',
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', '123456'),
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
