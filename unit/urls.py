4266fd26654d5d7010128b4b68ac8ec6ce36703b
Peter Yao <peteryao916@gmail.com>
2014-10-05 09:45:44 -0700

    Finalized charging people for todo's

--- unit/urls.py
+++ unit/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import patterns, include, url
 
 from . import views
 
-pk = r'(?P<unit_pk>\d+)'
+pk = r'(?P<todo_pk>\d+)'
 user_pk = r'(?P<user_pk>\d+)'
 
 urlpatterns = patterns('',
     url(r'^add_task/$', views.add_task, name='add_task'),
+    url(r'^finish_todo/{}/$'.format(pk), views.finish_todo, name='finish_todo'),
  )
--- unit/views.py
+++ unit/views.py
@@ -21,6 +21,47 @@ def add_task(request):
     debt_typedate_due = datetime.strptime(request.POST['date_due'], '%m/%d/%Y')
 
     bill = Bill(user=user, value=value, reason=reason, debt_typedate_due=debt_typedate_due, is_paid=False)
-    print bill
+    bill.save()
+
+    tenant = Tenant.objects.get(user=request.user.id)
+    unit_app = tenant.unit
+
+    for tenant in Tenant.objects.filter(unit=unit_app):
+        split = float(value) / (len(Tenant.objects.filter(unit=unit_app)) - 1)
+        has_paid = False
+        if tenant.user == request.user:
+            has_paid = True
+
+        final_split = Split_Bill(original=bill, user=user, split=split, has_paid=has_paid)
+        final_split.save()
+
+    return redirect('/')
+
+@login_required
+def finish_todo(request, todo_pk):
+    user = request.user
+    value = request.POST['amount']
+    reason = request.POST['name']
+    debt_typedate_due = datetime.strptime(request.POST['date_due'], '%m/%d/%Y')
+
+    bill = Bill(user=user, value=value, reason=reason, debt_typedate_due=debt_typedate_due, is_paid=False)
+    bill.save()
+
+    tenant = Tenant.objects.get(user=request.user.id)
+    unit_app = tenant.unit
+
+    todo = ToDoTask.objects.get(pk=todo_pk)
+    todo.is_complete = True
+    todo.save()
+
+    for tenant in Tenant.objects.filter(unit=unit_app):
+        split = float(value) / (len(Tenant.objects.filter(unit=unit_app)) - 1)
+        has_paid = False
+        if tenant.user == request.user:
+            has_paid = True
+
+        final_split = Split_Bill(original=bill, user=tenant.user, split=split, has_paid=has_paid)
+        print final_split
+        final_split.save()
 
     return redirect('/')
